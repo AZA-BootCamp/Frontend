31d45fa19b88b3f48a27410d8748798e588f8804
gmlwlsdl <heejin094@naver.com>
2024-08-01 16:21:02 +0900

    diagMore page modifing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer>
       <AZA
-        style={{ position: 'absolute', width: '137px', height: '137px', left: '300px', top: '82px' }}
+        style={{ position: 'absolute', width: '137px', height: '137px', left: '300px', top: '12px' }}
         style1={{ width: '137px', height: '57px', borderRadius: '120%' }}
         style2={{ fontSize: '32px', lineHeight: '40px' }}
       />
