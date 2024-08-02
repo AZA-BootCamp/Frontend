9623e4dc4b8e8df2cae110b173461db14c07f5d2
gmlwlsdl <heejin094@naver.com>
2024-08-02 15:15:21 +0900

    DiagMore modified

--- src/pages/DiagMore.js
+++ src/pages/DiagMore.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import image from '../assets/diag.png';
+import image_people from '../assets/diag_people.png';
+import Info from '../components/Info';
+import '../styles/DiagSimple.css';
+import NextBtn from '../components/NextBtn';
 
 const DiagMore = () => {
-  return <div>정밀 진단 Page</div>;
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+          }
+        });
+      },
+      {
+        threshold: 0.1, // 요소가 10% 이상 보일 때 콜백 실행
+      }
+    );
+
+    // 감시할 요소들 선택
+    const elements = document.querySelectorAll('.fade-element');
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+
+    return () => {
+      elements.forEach((element) => {
+        observer.unobserve(element);
+      });
+    };
+  }, []);
+
+  return (
+    <div className="diagsimple-group fade-element">
+      <div className="content fade-element">
+        <img src={image} alt="Background" className="diagsimple-group_5" />
+        <div className="diagsimple-group_1">
+          <div className="diagsmple-group_4"></div>
+          <div className="diagsimple-group_2">
+            <div className="diagsimple-group_3">
+              <img src={image_people} alt="People" className="diagsimple-group_6" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="line"></div>
+      <div className="info-container">
+        <Info text1="성별" />
+        <Info text1="키" />
+        <Info text1="몸무게" />
+        <Info text1="Brand" />
+      </div>
+      <div className="diagsimple-title">
+        <p className="diagsimple-title_text">정밀 진단</p>
+        <div className="diagsimple-title_box"></div>
+      </div>
+      <p className="diagsimple-detail">기본 정보 입력</p>
+      <NextBtn
+        style1={{
+          width: '75px',
+          height: '47px',
+          left: '1563px',
+          top: '695px',
+        }}
+        navigateTo="/diagMore2"
+      />
+    </div>
+  );
 };
 
 export default DiagMore;
