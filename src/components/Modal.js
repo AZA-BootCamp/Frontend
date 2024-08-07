492c5a6819e8fc999c6addfacfe3674001e7fc9b
gmlwlsdl <heejin094@naver.com>
2024-08-07 18:31:55 +0900

    fetch backend /routers/brands

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import ModalOption from './ModalOption';
 import '../styles/Modal.css';
 
-const Modal = ({ isOpen, onClose, options }) => {
+const Modal = ({ isOpen, onClose }) => {
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const fetchBrands = async () => {
+      const response = await fetch('http://127.0.0.1:8000/brands');
+      const data = await response.json();
+      setOptions(data);
+    };
+
+    if (isOpen) {
+      fetchBrands();
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   const modalHeight = 150 + options.length * 55; // 기본 높이 + 옵션 개수에 따른 높이 조정
