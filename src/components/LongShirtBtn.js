a8da9fce53e69ef06fc5f3c29a6a22bdeaffaf1e
gmlwlsdl <heejin094@naver.com>
2024-08-09 17:08:33 +0900

    category-btn modified

---
+++ src/components/LongShirtBtn.js
@@ -0,0 +1,15 @@
+// Header.js
+import React from 'react';
+import '../styles/CategoryBtn.css';
+
+const LongShirtBtn = () => {
+  return (
+    <div className="category-btn_group">
+      <button className="category-btn_box">
+        <p className="category-btn_text">긴팔</p>
+      </button>
+    </div>
+  );
+};
+
+export default LongShirtBtn;
